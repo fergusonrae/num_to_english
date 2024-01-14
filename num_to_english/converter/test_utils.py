d1d4aa61275aeb063bdc710e8a5e7e5fd6cd6177
Rachael Ferguson <fergusonrae@outlook.com>
2024-01-13 20:53:27 -0700

    Add initial unit tests to verify function works as expected.

--- num_to_english/converter/test_utils.py
+++ num_to_english/converter/test_utils.py
@@ -1,4 +1,13 @@
+import pytest
 from .utils import convert_number_to_english
 
-def test_convert_number_to_english():
-    pass
+
+@pytest.mark.parametrize("input,expected", [
+    (1, "one"),
+    (12345678, "twelve million three hundred forty five thousand six hundred seventy eight"),
+    (0.91233, "zero point nine one two three three"),
+    (0.00000000, "zero"),
+    (-1.01, "negative one point zero one"),
+])
+def test_convert_number_to_english(input, expected):
+    assert convert_number_to_english(input) == expected
