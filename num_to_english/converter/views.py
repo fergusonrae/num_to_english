e2044329bbc10615e0fc6d54839f68c2a3a00cfe
Rachael Ferguson <fergusonrae@outlook.com>
2024-01-13 22:36:27 -0700

    Fill out get and post endpoints.

--- num_to_english/converter/views.py
+++ num_to_english/converter/views.py
@@ -1,13 +1,25 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST
+from .utils import convert_number_to_english
 
 
 @require_GET
 def num_to_english_get(request):
-    return JsonResponse({'status': 'error', 'message': 'Not Implemented'})
+    return num_to_english_return(request.GET.get('number'))
 
 
 @require_POST
 def num_to_english_post(request):
-    return JsonResponse({'status': 'error', 'message': 'Not Implemented'})
+    return num_to_english_return(request.POST.get('number'))
+
+
+def num_to_english_return(request_number: str):
+    try:
+        if not request_number:
+            return JsonResponse({'status': 'error', 'message': 'Number must not be null'})
+        number = float(request_number)
+        number_in_english = convert_number_to_english(number)
+        response_data = {'status': 'ok', 'num_in_english': number_in_english}
+    except ValueError:
+        return JsonResponse({'status': 'error', 'message': 'Invalid number'})
+    return JsonResponse(response_data)
